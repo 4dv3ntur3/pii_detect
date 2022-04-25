733e02d6d0b1a5da4492f12cc51055aae6e3850d
4dv3ntur3 <allyep13.07@gmail.com>
2022-04-25 17:23:43 +0900

    2022-04-25 updated predict.py for print per word

--- predict_sample_2.py
+++ predict_sample_2.py
@@ -9,8 +9,8 @@ preds_list = [['AD_CITY-I', 'AD_CITY-I', 'AD_ADDRESS-I', 'AD_ADDRESS-I', 'AD_ADD
               ['AD_CITY-I', 'AD_CITY-I', 'AD_ADDRESS-B', 'AD_ADDRESS-I', 'AD_ADDRESS-I', 'AD_DETAIL-I', 'AD_ADDRESS-I', 'AD_DETAIL-I', 'AD_DETAIL-I', 'AD_DETAIL-I', 'AD_DETAIL-I', 'AD_DETAIL-I', 'AD_DETAIL-I', 'AD_DETAIL-I', 'AD_DETAIL-I', 'AD_DETAIL-I', 'AD_DETAIL-I']]
 
 
-all_input_tokens = [['010', '-', '236', '##9', '-', '47', '##89', '##로', '연락', '##주', '##세요', '.', '[SEP]']]
-preds_list = [['ID_PHONE-I', 'ID_PHONE-I', 'ID_PHONE-I', 'ID_PHONE-I', 'ID_PHONE-I', 'ID_PHONE-I', 'ID_PHONE-I', 'O', 'O', 'O', 'O', 'O']]
+# all_input_tokens = [['010', '-', '236', '##9', '-', '47', '##89', '##로', '연락', '##주', '##세요', '.', '[SEP]']]
+# preds_list = [['ID_PHONE-I', 'ID_PHONE-I', 'ID_PHONE-I', 'ID_PHONE-I', 'ID_PHONE-I', 'ID_PHONE-I', 'ID_PHONE-I', 'O', 'O', 'O', 'O', 'O']]
 
 
 
@@ -25,43 +25,65 @@ with open('./sample.txt', "w", encoding="utf-8") as f:
     for words, preds in zip(all_input_tokens, preds_list):
         
         line = ""
-        labels = []
-        words = []
         
         ahead_tag = ""
-        ahead_sen = ""
         pii_word = ""
+        
+        # print(words)
+        # print(preds)
+        
+        final_idx = (len(preds) if len(words) > len(preds) else len(words))-1
         
         for i, (word, pred) in enumerate(zip(words, preds)):
             
+            pred = pred.split("-")[0]
+            
+            if i == 0:
+                ahead_tag = pred
+            
             # 단어에 #이 있으면 이전 라벨과 비교한다 
             # word: 이전 토큰의 라벨과 동일하면 pii_word에 합치고, 아니라면 합치지 않는다 
-            # label: pii_word에 들어오는 가장 첫 토큰의 라벨로 한다. 
-
+ 
+            # 앞 단어와 이어짐
             if '#' in word:
+                
+                word = word.strip('#')
                 
                 if pred == ahead_tag:
                     pii_word += word
                     
-                else:
-                    line = line + word + " "
-                    pii = ""
-                    
-                    
+                else: # 다른 tag인 경우
+                    # 일단 이전 것까지는 작성한다
+                    if ahead_tag == 'O':
+                        line += pii_word
+                    else:
+                        line = line + "[{}:{}] ".format(pii_word, ahead_tag)
+                        
+                    pii_word = word
+                
+            # 이어지지 않음 
             else:
+                
                 if pred == ahead_tag:
-                    pii_word += word
+                    pii_word = pii_word + " " + word
                     
+                # 한 단어도 아니고 이어지지도 않는다 
                 else:
-                    line = line + word + " "
-                    pii = ""
+                    if ahead_tag == 'O':
+                        line += pii_word
+                    else:
+                        line = line + "[{}:{}] ".format(pii_word, ahead_tag)
+                    # line = line + word
+                    # pii_word = """
+                    pii_word = word
                     
-        
-        
-        
-            ahead_sen = word
             ahead_tag = pred
             
-                  
+            if i == final_idx:
+                if pred == 'O':
+                    line += pii_word
+                else:
+                    line = line + "[{}:{}] ".format(pii_word, ahead_tag)
+                
         f.write("{}\n".format(line.strip()))
         
